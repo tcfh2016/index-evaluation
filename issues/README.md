e40b21bdcdc3913a10d48d30bb48809752611d3d
tcfh2016 <tcfh2016@gmail.com>
2022-01-04 06:38:21 +0800

    resolve the 403 issue.

--- issues/README.md
+++ issues/README.md
@@ -9,6 +9,11 @@ r = requests.get('https://danjuanfunds.com/djmodule/value-center?channel=1300100
 print(r.status_code ) # 403
 ```
 
+After read [Python Requests.get访问网页403错误](https://zhuanlan.zhihu.com/p/35853860), seems the server can detect if the request is from a web scraper or not, so we need to build one header to simulate the real web browser.
+
+The more content about the header building can be also found here [定制请求头](https://docs.python-requests.org/zh_CN/latest/user/quickstart.html#id6)。
+
+
 Reference:
 
 - [HTTP 状态码](https://www.runoob.com/http/http-status-codes.html)
--- src/danjuan.py
+++ src/danjuan.py
@@ -1,6 +1,16 @@
 import requests
+from bs4 import BeautifulSoup
 
-r = requests.get('https://danjuanfunds.com/djmodule/value-center?channel=1300100141')
-#r = requests.get('https://www.baidu.com')
+headers = {
+'Accept': '*/*',
+'Accept-Encoding': 'gzip, deflate, br',
+'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
+'Connection': 'keep-alive',
+'host': 'danjuanfunds.com',
+'Referer': 'https://danjuanfunds.com/djmodule/value-center?channel=1300100141',
+'User-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW 64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36 QIHU 360SE'
+}
 
-print(r.status_code )
+r = requests.get('https://danjuanfunds.com/djmodule/value-center?channel=1300100141', headers=headers)
+soup = BeautifulSoup(r.text, 'html.parser')
+print(soup)
